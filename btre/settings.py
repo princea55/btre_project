9ae1a774546923be94beff54c72750a91e78a5f7
Prince <princesuriya14@gmail.com>
2019-09-02 09:03:53 +0530

    two new app

--- btre/settings.py
+++ btre/settings.py
@@ -1,5 +1,4 @@
 
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -119,3 +118,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'btre/static')
 ]
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
