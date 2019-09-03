2f0406527fcb4a48b389f16cb32cf75cd162a0c4
Prince <princesuriya14@gmail.com>
2019-09-03 10:33:59 +0530

    pull data fro database

--- listings/models.py
+++ listings/models.py
@@ -4,6 +4,7 @@ from realtors.models import Realtor
 
 
 class Listing(models.Model):
+    objects = None
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
